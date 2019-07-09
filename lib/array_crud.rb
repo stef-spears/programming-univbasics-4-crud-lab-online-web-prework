9a623d535a9ad57eea44c6b4edcd04561fe6bd9e
Stef Spears <spears.sj87@gmail.com>
2019-07-09 06:14:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+  p []
 end
 
 def create_an_array
-  ["snake", "in", "my", "boots"]
+  p ["snake", "in", "my", "boots"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -11,29 +11,29 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+  p array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  p array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  p array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  p array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  p array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  p array[index_number] = element
 end
